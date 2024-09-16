d23ee5ae581be10644b13242196eec6ce33e479f
Jessica Frank <kittentwix@LAPTOP-J7SSQI5S>
2024-09-16 10:54:05 -0400

    Here we go again, back to the beginning, back to when the world made sense.

---
+++ rainbow/src/main/java/com/jessicafrank/rainbow/RainbowApplication.java
@@ -0,0 +1,14 @@
+package com.jessicafrank.rainbow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainbowApplication.class, args);
+		System.out.println("Rainbow");
+		}
+
+}
